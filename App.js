985c85155959a7b756559926cc57db028c664f45
YJ Soon <yjsoon@tinkertanker.com>
2023-10-07 16:30:56 +0800

    Added add button and placeholder details screen

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
+import DetailsScreen from "./components/DetailsScreen";
 
 function HomeScreen() {
   const [colorArray, setColorArray] = useState([
-    { red: 128, green: 0, blue: 0, id: 0 },
-    { red: 0, green: 100, blue: 0, id: 1 },
+    { red: 255, green: 0, blue: 0, id: 0 },
+    { red: 0, green: 255, blue: 0, id: 1 },
     { red: 0, green: 0, blue: 255, id: 2 }
   ]);
 
@@ -16,8 +17,27 @@ function HomeScreen() {
     return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
   }
 
+  function addColor() {
+    // construct a new array with the old elements + a new one
+    const newColorArray = [
+      ...colorArray,
+      {
+        red: Math.floor(Math.random() * 256),
+        green: Math.floor(Math.random() * 256),
+        blue: Math.floor(Math.random() * 256),
+        id: colorArray.length
+      }
+    ];
+    setColorArray(newColorArray);
+  }
+
   return (
     <View style={styles.container}>
+      <Pressable
+        style={{ height: 40, justifyContent: "center" }}
+        onPress={addColor}>
+        <Text style={{ color: "red" }}>Add new color</Text>
+      </Pressable>
       <FlatList
         style={{ width: "100%" }}
         data={colorArray}
@@ -35,6 +55,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
